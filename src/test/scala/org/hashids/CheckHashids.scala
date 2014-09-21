e6e278474de83b0f51c1c7c45418b5aab7fc578f
John Ky <newhoggy@gmail.com>
2014-09-21 19:53:32 +1000

    Make syntax support available in library: toHashid and fromHashid methods.

--- src/test/scala/org/hashids/CheckHashids.scala
+++ src/test/scala/org/hashids/CheckHashids.scala
@@ -1,38 +1,33 @@
 package org.hashids
 
-import org.specs2._
+import org.hashids.HashidsSyntax._
 import org.scalacheck._
+import org.specs2._
 
 case class ZeroOrPosLong(value: Long)
 
 class CheckHashids extends org.specs2.Specification with org.specs2.ScalaCheck {
   import CheckHashids.arbitraryZeroOrPosLong
 
-  implicit val hashid = Hashids("salt")
-
   def is = {
-    "List of random zero or positive longs should encode then decode" ! check { (a: List[ZeroOrPosLong]) => a.raw.toHashid.fromHashid must_== a.raw } ^ end
+    "List of random zero or positive longs should encode then decode" ! {
+      check { (a: List[ZeroOrPosLong], salt: String) =>
+        implicit val hashid = Hashids(salt)
+
+        a.raw.toHashid.fromHashid must_== a.raw
+      }
+    } ^ {
+      end
+    }
   }
 
   implicit class RichListZeroOrPosLong(self: List[ZeroOrPosLong]) {
     def raw = self.map(_.value)
   }
-
-  implicit class RichListInt(self: List[Long]) {
-    def toHashid(implicit hashids: Hashids): String = {
-      hashids.encode(self: _*)
-    }
-  }
-
-  implicit class RichString(self: String) {
-    def fromHashid(implicit hashids: Hashids): List[Long] = {
-      hashids.decode(self).toList
-    }
-  }
 }
 
 object CheckHashids {
   implicit val arbitraryZeroOrPosLong: Arbitrary[ZeroOrPosLong] = Arbitrary {
-    Gen.chooseNum(0L, Long.MaxValue).map(ZeroOrPosLong(_))
+    Gen.chooseNum(0L, Long.MaxValue, 2L, 75527867232L).map(ZeroOrPosLong(_))
   }
 }
