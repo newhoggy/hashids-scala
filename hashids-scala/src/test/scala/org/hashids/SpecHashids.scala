2cf9bc6e94529b7cdbd9f73f701b384fe3d30263
John Ky <newhoggy@gmail.com>
2015-10-30 20:30:13 +1100

    PR2: Re-enable tests.

--- hashids-scala/src/test/scala/org/hashids/SpecHashids.scala
+++ hashids-scala/src/test/scala/org/hashids/SpecHashids.scala
@@ -5,132 +5,132 @@ import org.specs2.mutable._
 import org.hashids.impl._
 
 class SpecHashids extends Specification {
-  // "One number should encode then decode" >> {
-  //   implicit val hashids = Hashids("this is my salt")
-  //   val expected = "NkK9"
-  //   val data = 12345L
-  //   val hashed = data.hashid
-  //   val unhashed = hashed.unhashid
-  //   hashed must_== expected
-  //   unhashed must_== List(data)
-  // }
+   "One number should encode then decode" >> {
+     implicit val hashids = Hashids("this is my salt")
+     val expected = "NkK9"
+     val data = 12345L
+     val hashed = data.hashid
+     val unhashed = hashed.unhashid
+     hashed must_== expected
+     unhashed must_== List(data)
+   }
 
-  // "Several numbers should encode then decode" >> {
-  //   implicit val hashids = Hashids("this is my salt")
-  //   val expected = "aBMswoO2UB3Sj"
-  //   val data = Seq[Long](683L, 94108L, 123L, 5L)
-  //   val hashed = data.hashid
-  //   val unhashed = hashed.unhashid
-  //   hashed must_== expected
-  //   unhashed must_== data
-  // }
+   "Several numbers should encode then decode" >> {
+     implicit val hashids = Hashids("this is my salt")
+     val expected = "aBMswoO2UB3Sj"
+     val data = Seq[Long](683L, 94108L, 123L, 5L)
+     val hashed = data.hashid
+     val unhashed = hashed.unhashid
+     hashed must_== expected
+     unhashed must_== data
+   }
 
-  // "One number should encode then decode with salt" >> {
-  //   implicit val hashids = Hashids("this is my salt", 8)
-  //   val expected = "gB0NV05e"
-  //   val data = 1L
-  //   val hashed = data.hashid
-  //   val unhashed = hashed.unhashid
-  //   hashed must_== expected
-  //   unhashed must_== List(data)
-  // }
+   "One number should encode then decode with salt" >> {
+     implicit val hashids = Hashids("this is my salt", 8)
+     val expected = "gB0NV05e"
+     val data = 1L
+     val hashed = data.hashid
+     val unhashed = hashed.unhashid
+     hashed must_== expected
+     unhashed must_== List(data)
+   }
 
-  // "Require alphabet with at least 16 unique chars" in {
-  //   new Hashids(
-  //     salt = "this is my salt",
-  //     alphabet = "1123467890abcde"
-  //   ) must throwA[IllegalArgumentException](
-  //     message = "alphabet must contain at least 16 unique characters")
-  // }
+   "Require alphabet with at least 16 unique chars" in {
+     new Hashids(
+       salt = "this is my salt",
+       alphabet = "1123467890abcde"
+     ) must throwA[IllegalArgumentException](
+       message = "alphabet must contain at least 16 unique characters")
+   }
 
-  // "Deny spaces in alphabet" in {
-  //   new Hashids(
-  //     salt = "this is my salt",
-  //     alphabet = "1234567890 abcdefghijklmnopqrstuvwxyz"
-  //   ) must throwA[IllegalArgumentException](
-  //     message = "alphabet cannot contains spaces")
-  // }
+   "Deny spaces in alphabet" in {
+     new Hashids(
+       salt = "this is my salt",
+       alphabet = "1234567890 abcdefghijklmnopqrstuvwxyz"
+     ) must throwA[IllegalArgumentException](
+       message = "alphabet cannot contains spaces")
+   }
 
-  // "Should be random" >> {
-  //   implicit val hashids = Hashids("this is my salt")
-  //   val expected = "1Wc8cwcE"
-  //   val data = List[Long](5L, 5L, 5L, 5L)
-  //   val hashed = data.hashid
-  //   val unhashed = hashed.unhashid
-  //   hashed must_== expected
-  //   unhashed must_== data
-  // }
+   "Should be random" >> {
+     implicit val hashids = Hashids("this is my salt")
+     val expected = "1Wc8cwcE"
+     val data = List[Long](5L, 5L, 5L, 5L)
+     val hashed = data.hashid
+     val unhashed = hashed.unhashid
+     hashed must_== expected
+     unhashed must_== data
+   }
 
-  // "Hash for sequence of consecutive numbers should appear random" >> {
-  //   implicit val hashids = Hashids("this is my salt")
-  //   val expected = "kRHnurhptKcjIDTWC3sx"
-  //   val data = List[Long](1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L)
-  //   val hashed = data.hashid
-  //   val unhashed = hashed.unhashid
-  //   hashed must_== expected
-  //   unhashed must_== data
-  // }
+   "Hash for sequence of consecutive numbers should appear random" >> {
+     implicit val hashids = Hashids("this is my salt")
+     val expected = "kRHnurhptKcjIDTWC3sx"
+     val data = List[Long](1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L)
+     val hashed = data.hashid
+     val unhashed = hashed.unhashid
+     hashed must_== expected
+     unhashed must_== data
+   }
 
-  // "Sequence of hashes for consecutive numbers should appear random" >> {
-  //   implicit val hashids = Hashids("this is my salt")
-  //   1L.hashid must_== "NV"
-  //   2L.hashid must_== "6m"
-  //   3L.hashid must_== "yD"
-  //   4L.hashid must_== "2l"
-  //   5L.hashid must_== "rD"
-  // }
+   "Sequence of hashes for consecutive numbers should appear random" >> {
+     implicit val hashids = Hashids("this is my salt")
+     1L.hashid must_== "NV"
+     2L.hashid must_== "6m"
+     3L.hashid must_== "yD"
+     4L.hashid must_== "2l"
+     5L.hashid must_== "rD"
+   }
 
-  // "Max long value should encode" >> {
-  //   implicit val hashids = Hashids("this is my salt")
-  //   9876543210123L.hashid must_== "Y8r7W1kNN"
-  // }
+   "Max long value should encode" >> {
+     implicit val hashids = Hashids("this is my salt")
+     9876543210123L.hashid must_== "Y8r7W1kNN"
+   }
 
-  // "Special number encode and decode" >> {
-  //   implicit val hashids = Hashids("this is my salt")
-  //   val expected = "3kK3nNOe"
-  //   val data = 75527867232L
-  //   val hashed = data.hashid
-  //   val unhashed = hashed.unhashid
-  //   hashed must_== expected
-  //   unhashed must_== List(data)
-  // }
+   "Special number encode and decode" >> {
+     implicit val hashids = Hashids("this is my salt")
+     val expected = "3kK3nNOe"
+     val data = 75527867232L
+     val hashed = data.hashid
+     val unhashed = hashed.unhashid
+     hashed must_== expected
+     unhashed must_== List(data)
+   }
 
-  // "Decoding with different salt result's in empty list" >> {
-  //   val hashid = Hashids("this is my salt")
-  //   val hash = hashid.encode(10L)
+   "Decoding with different salt result's in empty list" >> {
+     val hashid = Hashids("this is my salt")
+     val hash = hashid.encode(10L)
 
-  //   Hashids("different salt").decode(hash) must_== Nil
-  // }
+     Hashids("different salt").decode(hash) must_== Nil
+   }
 
-  // "encodeHex" >> {
-  //   val hashids = Hashids("this is my salt")
+   "encodeHex" >> {
+     val hashids = Hashids("this is my salt")
 
-  //   "encodes hex string" >> {
-  //     hashids.encodeHex("FA"         ) must_== "lzY"
-  //     hashids.encodeHex("26dd"       ) must_== "MemE"
-  //     hashids.encodeHex("FF1A"       ) must_== "eBMrb"
-  //     hashids.encodeHex("12abC"      ) must_== "D9NPE"
-  //     hashids.encodeHex("185b0"      ) must_== "9OyNW"
-  //     hashids.encodeHex("17b8d"      ) must_== "MRWNE"
-  //     hashids.encodeHex("1d7f21dd38" ) must_== "4o6Z7KqxE"
-  //     hashids.encodeHex("20015111d"  ) must_== "ooweQVNB"
-  //   }
+     "encodes hex string" >> {
+       hashids.encodeHex("FA"         ) must_== "lzY"
+       hashids.encodeHex("26dd"       ) must_== "MemE"
+       hashids.encodeHex("FF1A"       ) must_== "eBMrb"
+       hashids.encodeHex("12abC"      ) must_== "D9NPE"
+       hashids.encodeHex("185b0"      ) must_== "9OyNW"
+       hashids.encodeHex("17b8d"      ) must_== "MRWNE"
+       hashids.encodeHex("1d7f21dd38" ) must_== "4o6Z7KqxE"
+       hashids.encodeHex("20015111d"  ) must_== "ooweQVNB"
+     }
 
-  //   "throw if non-hex string passed" >> {
-  //     hashids.encodeHex("XYZ123") must throwA[IllegalArgumentException](
-  //       message = "Not a HEX string")
-  //   }
-  // }
+     "throw if non-hex string passed" >> {
+       hashids.encodeHex("XYZ123") must throwA[IllegalArgumentException](
+         message = "Not a HEX string")
+     }
+   }
 
-  // "decodeHex" >> {
-  //   val hashids = Hashids("this is my salt")
+   "decodeHex" >> {
+     val hashids = Hashids("this is my salt")
 
-  //   "decodes hex string" >> {
-  //     hashids.decodeHex("lzY"    ) must_== "FA"
-  //     hashids.decodeHex("eBMrb"  ) must_== "FF1A"
-  //     hashids.decodeHex("D9NPE"  ) must_== "12ABC"
-  //   }
-  // }
+     "decodes hex string" >> {
+       hashids.decodeHex("lzY"    ) must_== "FA"
+       hashids.decodeHex("eBMrb"  ) must_== "FF1A"
+       hashids.decodeHex("D9NPE"  ) must_== "12ABC"
+     }
+   }
 
   "Should be compatible with Javascript reference implementation" >> {
     "for salt = 'MyCamelCaseSalt', max length = 10, alphabet = 'ABCDEFGHIJKLMNPQRSTUVWXYZ123456789'" >> {
